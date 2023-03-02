28ad27f54f5d1bd360f345f0c45b3c54b0967f2a
donatomontrone <donato.montrone@outlook.com>
2023-03-02 20:56:10 +0100

    added project in main Component

---
+++ src/components/MainComponent.vue
@@ -0,0 +1,35 @@
+<template>
+  <main v-for="project in projects"> {{ project.name }} </main>
+</template>
+<script>
+import axios from 'axios';
+export default {
+  name: 'AppMain',
+  data() {
+    return {
+      projects: [],
+      apiUrl: 'http://127.0.0.1:8000/api/projects/'
+    }
+  },
+
+  methods: {
+    getProjects() {
+      axios.get(this.apiUrl, {
+        params: {
+        }
+      })
+        .then((response) => {
+          console.log(response.data.results.data);
+          this.projects = response.data.results.data;
+        })
+        .catch(function (error) {
+          console.error(error);
+        });
+    }
+  },
+  created() {
+    this.getProjects();
+  }
+}
+</script>
+<style lang="scss" scoped></style>

4c66d47182f7d62ace2c9d273bf3e88cbd31b6cc
donatomontrone <donato.montrone@outlook.com>
2023-03-02 22:38:49 +0100

    edit index.html and added simple pagination

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -2,7 +2,15 @@
   <main>
     <div class="container">
       <div class="row">
+        <div class="navigation-buttons d-flex justify-content-between mb-3">
+          <a href="#!" @click="prevPage()" class="btn btn-outline-dark">Prev</a>
+          <a href="#!" @click="nextPage()" class="btn btn-outline-dark">Next</a>
+        </div>
         <ProjectCard v-for="project in projects" :projectInfo="project" />
+        <div class="navigation-buttons d-flex justify-content-between mt.3">
+          <a href="#" @click="prevPage()" class="btn btn-outline-dark">Prev</a>
+          <a href="#" @click="nextPage()" class="btn btn-outline-dark">Next</a>
+        </div>
       </div>
     </div>
   </main>
@@ -18,21 +26,38 @@ export default {
   data() {
     return {
       projects: [],
-      apiUrl: "http://127.0.0.1:8000/api/projects/"
+      apiUrl: "http://127.0.0.1:8000/api/projects/",
+      page: 1,
+      lastPage: '',
     };
   },
   methods: {
     getProjects() {
       axios.get(this.apiUrl, {
-        params: {}
+        params: {
+          page: this.page,
+        }
       })
         .then((response) => {
           console.log(response.data.results.data);
           this.projects = response.data.results.data;
+          this.lastPage = response.data.results.last_page;
         })
         .catch(function (error) {
           console.error(error);
         });
+    },
+    prevPage() {
+      if (this.page > 1) {
+        this.page -= 1
+        this.getProjects()
+      }
+    },
+    nextPage() {
+      if (this.page < this.lastPage) {
+        this.page += 1
+        this.getProjects();
+      }
     }
   },
   created() {
--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -4,19 +4,24 @@
             <div class="row">
                 <div class="col-12"></div>
                 <div class="col-md-4">
-                    <img :src="projectInfo.preview" class="img-fluid rounded-start" :alt="projectInfo.names">
+                    <img v-if="projectInfo.preview.startsWith('img')"
+                        :src="'http://127.0.0.1:8000/storage/' + projectInfo.preview" class="img-fluid rounded-start"
+                        :alt="projectInfo.names">
+                    <img v-else :src="projectInfo.preview" class="img-fluid rounded-start" :alt="projectInfo.names">
                 </div>
                 <div class="col-md-8">
                     <div class="card-body">
-                        <h6 class="badge bg-secondary">{{ projectInfo.type.name }}</h6>
+                        <h6 class="badge bg-secondary">{{ projectInfo?.type?.name || 'Empty' }}</h6>
                         <h5 class="card-title">{{ projectInfo.name }}</h5>
                         <p class="card-text">GitHub URL: <a :href="projectInfo.github_url">{{ projectInfo.github_url }}</a>
                         </p>
                         <p class="card-text">Difficulty: {{ projectInfo.difficulty_id }}/5</p>
                         <p class="card-text"><small class="text-muted"> Published on {{ projectInfo.publication_date
                         }}</small></p>
-                        <span v-for="technology in projectInfo.technologies" class="badge rounded-pill me-1"
-                            :style="{ 'background-color': technology.color }">#{{ technology.name }}</span>
+                        <span v-if="projectInfo.technologies.length > 0" v-for="technology in projectInfo.technologies"
+                            class="badge rounded-pill me-1" :style="{ 'background-color': technology.color }">#{{
+                                technology.name }}</span>
+                        <span v-else class="badge rounded-pill text-bg-warning"> NO TAGS</span>
                     </div>
                 </div>
             </div>
